a2337db9ccccdbc2efaccf0bc79fc8d2ad491016
zygmunman <jmart237@gmail.com>
2021-06-12 17:07:48 +0200

    Commit 18-bis: Seeders de Menús

--- database/seeders/MenuRolSeeder.php
+++ database/seeders/MenuRolSeeder.php
@@ -19,7 +19,13 @@ class MenuRolSeeder extends Seeder
             array('menu_id' => '3', 'rol_id' => '1'),
             array('menu_id' => '2', 'rol_id' => '1'),
             array('menu_id' => '1', 'rol_id' => '1'),
-            array('menu_id' => '4', 'rol_id' => '1')
+            array('menu_id' => '4', 'rol_id' => '1'),
+            array('menu_id' => '5', 'rol_id' => '1'),
+            array('menu_id' => '6', 'rol_id' => '1'),
+            array('menu_id' => '7', 'rol_id' => '1'),
+            array('menu_id' => '8', 'rol_id' => '1'),
+            array('menu_id' => '9', 'rol_id' => '1'),
+            array('menu_id' => '10', 'rol_id' => '1')
         );
 
         DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -16,10 +16,17 @@ class MenuSeeder extends Seeder
     public function run()
     {
         $menu = array(
-            array('id' => '1','menu_id' => NULL,'nombre' => 'Navegación','url' => 'javascript:;','orden' => '2','icono' => NULL,'created_at' => '2021-01-31 17:15:14','updated_at' => '2021-02-11 22:09:15'),
-            array('id' => '2','menu_id' => '1','nombre' => 'Menú','url' => 'menu','orden' => '1','icono' => NULL,'created_at' => '2021-01-31 17:20:57','updated_at' => '2021-02-11 22:09:15'),
-            array('id' => '3','menu_id' => NULL,'nombre' => 'Dashboard','url' => 'dashboard','orden' => '1','icono' => NULL,'created_at' => '2021-01-31 17:21:55','updated_at' => '2021-02-11 22:09:15'),
-            array('id' => '4','menu_id' => '1','nombre' => 'Menu Rol','url' => 'menu-rol','orden' => '2','icono' => NULL,'created_at' => '2021-02-11 21:58:35','updated_at' => '2021-02-11 22:09:15')
+
+            array('id' => '1', 'menu_id' => NULL, 'nombre' => 'Navegación', 'url' => 'javascript:;', 'orden' => '2', 'icono' => NULL, 'created_at' => '2021-06-10 17:21:08', 'updated_at' => '2021-06-10 17:25:22'),
+            array('id' => '2', 'menu_id' => '1', 'nombre' => 'Menu', 'url' => 'menu', 'orden' => '1', 'icono' => NULL, 'created_at' => '2021-06-10 17:21:08', 'updated_at' => '2021-06-10 17:25:22'),
+            array('id' => '3', 'menu_id' => NULL, 'nombre' => 'Dashboard', 'url' => 'dashboard', 'orden' => '1', 'icono' => NULL, 'created_at' => '2021-06-10 17:21:08', 'updated_at' => '2021-06-10 17:25:22'),
+            array('id' => '4', 'menu_id' => '1', 'nombre' => 'Menu Rol', 'url' => 'menu-rol', 'orden' => '2', 'icono' => NULL, 'created_at' => '2021-06-10 17:21:08', 'updated_at' => '2021-06-10 17:25:22'),
+            array('id' => '5', 'menu_id' => NULL, 'nombre' => 'Seguridad', 'url' => 'javascript:;', 'orden' => '3', 'icono' => NULL, 'created_at' => '2021-06-10 17:22:14', 'updated_at' => '2021-06-10 17:25:22'),
+            array('id' => '6', 'menu_id' => '5', 'nombre' => 'Rol', 'url' => 'rol', 'orden' => '1', 'icono' => NULL, 'created_at' => '2021-06-10 17:22:28', 'updated_at' => '2021-06-10 17:25:22'),
+            array('id' => '7', 'menu_id' => '5', 'nombre' => 'Permiso', 'url' => 'permiso', 'orden' => '2', 'icono' => NULL, 'created_at' => '2021-06-10 17:22:50', 'updated_at' => '2021-06-10 17:25:22'),
+            array('id' => '8', 'menu_id' => '5', 'nombre' => 'Permiso Rol', 'url' => 'permiso-rol', 'orden' => '3', 'icono' => NULL, 'created_at' => '2021-06-10 17:23:12', 'updated_at' => '2021-06-10 17:25:22'),
+            array('id' => '9', 'menu_id' => NULL, 'nombre' => 'Post', 'url' => 'javascript:;', 'orden' => '4', 'icono' => NULL, 'created_at' => '2021-06-10 17:24:49', 'updated_at' => '2021-06-10 17:25:22'),
+            array('id' => '10', 'menu_id' => '9', 'nombre' => 'Categorías', 'url' => 'categoria', 'orden' => '1', 'icono' => NULL, 'created_at' => '2021-06-10 17:25:09', 'updated_at' => '2021-06-10 17:25:22')
           );
           DB::statement('SET FOREIGN_KEY_CHECKS = 0;');
           DB::table('menu')->truncate();
