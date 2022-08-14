7eeecd17e3358940aeb2cbab6edd5acd1b0d920e
zygmunman <jmart237@gmail.com>
2022-08-15 00:21:24 +0200

    Commint 21: Post I

--- resources/views/theme/back/app.blade.php
+++ resources/views/theme/back/app.blade.php
@@ -15,9 +15,9 @@
     <link href="{{asset("assets/back/libs/datatables.net-bs4/css/dataTables.bootstrap4.css")}}" rel="stylesheet">
     <link href="{{asset("assets/back/libs/toastr/build/toastr.min.css")}}" rel="stylesheet">
     <link href="{{asset("assets/back/extra-libs/sweetalert2/sweetalert2.min.css")}}" rel="stylesheet">
+    @yield("styles")
     <link href="{{asset("assets/back/css/style.min.css")}}" rel="stylesheet">
     <!--CSS DINAMICO-->
-    @yield("styles")
     <link href="{{asset("assets/back/css/tutoblog.css")}}" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
